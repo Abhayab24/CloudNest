dce5806b7486edc4f5d29e84c4980bcd001ecb83
Abhay Rana <abhaypartap542005@gmail.com>
2026-05-12 09:26:55 +0530

    backend deployed in render

--- backend/server.js
+++ backend/server.js
@@ -292,7 +292,8 @@ app.delete("/delete/:filename", auth, async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
 
-    console.log("Server running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
